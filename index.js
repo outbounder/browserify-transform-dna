dc9cd251921b91c2bef24cc0a736b3fbbb1e1162
Boris Filipov <obiwonn@gmail.com>
2014-06-21 14:36:08 +0300

    simplify and unify dna loading routine

--- index.js
+++ index.js
@@ -2,39 +2,24 @@ var fs = require('fs')
 var _ = require("underscore")
 var transformTools = require("browserify-transform-tools")
 
-var clientDNA = {}
-var clientDNALoaded = false
+var DNA = require("organic-dna")
+var fold = require("organic-dna-fold")
+var resolveReferences = require("organic-dna-resolvereferences")
 
-module.exports = transformTools.makeRequireTransform("requireTransform",
-  _.extend({evaluateArguments: false, jsFilesOnly: true}),
-  function(args, opts, cb) {
-    if (args[0] == '"dna"') {
-      if(!clientDNALoaded)
-        if(fs.existsSync(process.cwd()+"/dna")) {
-          var DNA = require("organic").DNA
-          var dna = new DNA()
-          dna.loadDir(process.cwd()+"/dna", function(){
-            if(dna.client) {
-              if(dna[process.env.CELL_MODE])
-                  dna.mergeBranchInRoot(process.env.CELL_MODE)
-              _.extend(clientDNA, dna.client)
-              clientDNALoaded = true
-              cb(null, JSON.stringify(clientDNA))
-            } else
-              cb(null, JSON.stringify(clientDNA))
-          })
-        } else
-        if(fs.existsSync(process.cwd()+"/jadeify.json")) {
-          clientDNA = require(process.cwd()+"/jadeify.json")
-          clientDNALoaded = true
-          cb(null, JSON.stringify(clientDNA))
-        } else {
-          clientDNALoaded = true
-          cb(null, JSON.stringify(clientDNA))
-        }
-      else
-        cb(null, JSON.stringify(clientDNA))
-    } else {
-      return cb();
-    }
-  })
+module.exports = transformTools.makeRequireTransform("requireTransform",{
+  evaluateArguments: false, 
+  jsFilesOnly: true
+}, function(args, opts, cb) {
+  if (args[0] == '"dna"') {
+    var dna = new DNA()
+    dna.loadDir(process.cwd()+"/dna", function(){
+      resolveReferences(dna)
+      if(dna[process.env.CELL_MODE])
+          fold(dna, process.env.CELL_MODE)
+
+      cb(null, JSON.stringify(dna.client))
+    })
+  } else {
+    return cb();
+  }
+})
